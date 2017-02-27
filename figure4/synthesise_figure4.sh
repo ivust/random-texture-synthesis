bf0a9ce4de4106787a8b0ebd4ffb36faa946e6e8
Ivan Ustyuzhaninov <ivanustyuzh@gmail.com>
2017-02-27 19:02:38 +0100

    Add synthesise_figure4.sh

---
+++ figure4/synthesise_figure4.sh
@@ -0,0 +1,60 @@
+python3 ../../synthesise.py -t textures/barbara.jpg -s 256 -f textures/barbara.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/barbara.jpg -s 256 -f textures/barbara.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/brick_wall.jpg -s 256 -f textures/brick_wall.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/brick_wall.jpg -s 256 -f textures/brick_wall.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/bubbly_0067.jpg -s 256 -f textures/bubbly_0067.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/bubbly_0067.jpg -s 256 -f textures/bubbly_0067.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/bubbly_0111.jpg -s 256 -f textures/bubbly_0111.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/bubbly_0111.jpg -s 256 -f textures/bubbly_0111.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/bumpy_0121.jpg -s 256 -f textures/bumpy_0121.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/bumpy_0121.jpg -s 256 -f textures/bumpy_0121.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/cobwebbed_0132.jpg -s 256 -f textures/cobwebbed_0132.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/cobwebbed_0132.jpg -s 256 -f textures/cobwebbed_0132.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/frilly_0103.jpg -s 256 -f textures/frilly_0103.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/frilly_0103.jpg -s 256 -f textures/frilly_0103.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/gauzy_0126.jpg -s 256 -f textures/gauzy_0126.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/gauzy_0126.jpg -s 256 -f textures/gauzy_0126.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/grid_0053.jpg -s 256 -f textures/grid_0053.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/grid_0053.jpg -s 256 -f textures/grid_0053.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/lacelike_0004.jpg -s 256 -f textures/lacelike_0004.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/lacelike_0004.jpg -s 256 -f textures/lacelike_0004.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/lacelike_0069.jpg -s 256 -f textures/lacelike_0069.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/lacelike_0069.jpg -s 256 -f textures/lacelike_0069.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/lacelike_0073.jpg -s 256 -f textures/lacelike_0073.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/lacelike_0073.jpg -s 256 -f textures/lacelike_0073.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/lena.jpg -s 256 -f textures/lena.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/lena.jpg -s 256 -f textures/lena.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/meshed_0164.jpg -s 256 -f textures/meshed_0164.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/meshed_0164.jpg -s 256 -f textures/meshed_0164.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/pebble_texture.jpg -s 256 -f textures/pebble_texture.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/pebble_texture.jpg -s 256 -f textures/pebble_texture.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/pebbles.jpg -s 256 -f textures/pebbles.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/pebbles.jpg -s 256 -f textures/pebbles.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/porous_0137.jpg -s 256 -f textures/porous_0137.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/porous_0137.jpg -s 256 -f textures/porous_0137.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/radish.jpg -s 256 -f textures/radish.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/radish.jpg -s 256 -f textures/radish.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/scaly_0127.jpg -s 256 -f textures/scaly_0127.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/scaly_0127.jpg -s 256 -f textures/scaly_0127.jpg-11.jpg --scales 11
+
+python3 ../../synthesise.py -t textures/trees.jpg -s 256 -f textures/trees.jpg-multiscale.jpg
+python3 ../../synthesise.py -t textures/trees.jpg -s 256 -f textures/trees.jpg-11.jpg --scales 11
+
